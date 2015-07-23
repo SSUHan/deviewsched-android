88e81533bb849c37126aedaf1d94b419ccd035e5
Hyunseol Kim <hyunseol518@naver.com>
2015-07-23 11:07:08 +0900

    Home UI를 그리기위한 Fragment 기본 세팅 // ViewPager 생성 // CoordinatorLayout 생성

--- app/src/main/java/com/gdgssu/android_deviewsched/ui/HomeFragment.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/ui/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.gdgssu.android_deviewsched.ui;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,11 +11,9 @@ import android.view.ViewGroup;
 
 import com.gdgssu.android_deviewsched.R;
 
-/**
- * Created by flashgugu on 15. 7. 20.
- */
 public class HomeFragment extends DeviewFragment {
     private Toolbar mToolbar;
+    private ViewPager mPager;
 
     private static final String APP_NAME_KEY = "APP_NANE";
     private CharSequence appName;
@@ -48,17 +47,24 @@ public class HomeFragment extends DeviewFragment {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
         initToolbar(rootView);
+        initFragmentPager(rootView);
 
         return rootView;
     }
 
     private void initToolbar(View rootView) {
-
         mToolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
         ((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
 
-        final ActionBar ab = ((AppCompatActivity)getActivity()).getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
-        ab.setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initFragmentPager(View rootView) {
+        mPager = (ViewPager) rootView.findViewById(R.id.content_pager);
+        PagerFragmentAdapter adpater = new PagerFragmentAdapter(
+                getChildFragmentManager(), getActivity());
+        mPager.setAdapter(adpater);
     }
 }
---
+++ app/src/main/java/com/gdgssu/android_deviewsched/ui/PagerFragmentAdapter.java
@@ -0,0 +1,27 @@
+package com.gdgssu.android_deviewsched.ui;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PagerFragmentAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+
+    public PagerFragmentAdapter(FragmentManager fm, Context context) {
+        super(fm);
+
+        this.mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
