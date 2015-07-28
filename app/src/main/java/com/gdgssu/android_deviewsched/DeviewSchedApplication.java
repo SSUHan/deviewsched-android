c87e52560fe3088ed4d612bd4f29672b57e4dae6
Hyunseol Kim <hyunseol518@naver.com>
2015-07-28 19:58:36 +0900

    facebook로그인 기능이 포함된 애플리케이션 SplashActivity와 로그인 로직 개발 (로그아웃 미개발)

--- app/src/main/java/com/gdgssu/android_deviewsched/DeviewSchedApplication.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/DeviewSchedApplication.java
@@ -2,19 +2,26 @@ package com.gdgssu.android_deviewsched;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
+import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
+import com.gdgssu.android_deviewsched.helper.LoginPreferenceHelper;
 
 public class DeviewSchedApplication extends Application{
 
     public static Context GLOBAL_CONTEXT;
+    public static Boolean LOGIN_STATE;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         GLOBAL_CONTEXT = getApplicationContext();
+        LoginPreferenceHelper prefHelper = new LoginPreferenceHelper(GLOBAL_CONTEXT);
+        LOGIN_STATE = prefHelper.getPrefLoginValue(LoginPreferenceHelper.PREF_LOGIN_STATE, false);
 
         FacebookSdk.sdkInitialize(GLOBAL_CONTEXT);
+
     }
 }
