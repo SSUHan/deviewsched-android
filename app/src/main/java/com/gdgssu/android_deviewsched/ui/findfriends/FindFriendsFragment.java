0d36b8a5efacecca27ffb42a5e3ad0ae061ac258
SSUHan <joyangel93@naver.com>
2015-08-05 10:54:44 +0900

    친구찾기 pulltorefresh 안드로이드 기본뷰나오게 완성, 커스텀화 시작할것

--- app/src/main/java/com/gdgssu/android_deviewsched/ui/findfriends/FindFriendsFragment.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/ui/findfriends/FindFriendsFragment.java
@@ -1,29 +1,41 @@
 package com.gdgssu.android_deviewsched.ui.findfriends;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.gdgssu.android_deviewsched.R;
 import com.gdgssu.android_deviewsched.model.FindFriend;
 import com.gdgssu.android_deviewsched.ui.DeviewFragment;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
+
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 
-public class FindFriendsFragment extends DeviewFragment implements SwipeRefreshLayout.OnRefreshListener {
+public class FindFriendsFragment extends DeviewFragment {
 
     private static final String KEY_TITLE = "title";
     private CharSequence title;
 
-    private ArrayList<FindFriend> findFriendses;
+    private LinkedList<String> FriendList;
+    private String[] mStrings = {"Joun","Bob","Chaen","Davide","Elem"};
 
-    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private PullToRefreshListView mPullRefreshListView;
+    private ArrayAdapter<String> mAdapter;
 
     public static FindFriendsFragment newInstance(CharSequence title) {
         FindFriendsFragment fragment = new FindFriendsFragment();
@@ -52,34 +64,81 @@ public class FindFriendsFragment extends DeviewFragment implements SwipeRefreshL
 
         View rootView = inflater.inflate(R.layout.fragment_find_friends, container, false);
 
-        initSwipeRefreshLayout(rootView);
+        initPullRefresh(rootView);
         initList(rootView);
 
         return rootView;
     }
 
-    private void initSwipeRefreshLayout(View rootView) {
-        mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_container);
-        mSwipeRefreshLayout.setOnRefreshListener(this);
-        mSwipeRefreshLayout.setColorSchemeColors(Color.RED, Color.BLUE, Color.BLACK);
+    private void initPullRefresh(View rootView){
+        mPullRefreshListView = (PullToRefreshListView)rootView.findViewById(R.id.pull_refresh_list);
+        mPullRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                String label = DateUtils.formatDateTime(getActivity(), System.currentTimeMillis(),
+                        DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+                refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
+
+
+                new GetDataTask().execute();
+
+
+            }
+        });
+        mPullRefreshListView.setOnLastItemVisibleListener(new PullToRefreshBase.OnLastItemVisibleListener() {
+            @Override
+            public void onLastItemVisible() {
+                Toast.makeText(getActivity(), "End of List!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
     private void initList(View rootView) {
 
-        ListView mListView = (ListView)rootView.findViewById(R.id.listview);
-        FindFriendsAdapter adapter = new FindFriendsAdapter(findFriendses);
-        mListView.setAdapter(adapter);
-    }
+        ListView actualListView = mPullRefreshListView.getRefreshableView();
+        registerForContextMenu(actualListView);
 
-    @Override
-    public void onRefresh() {
-        Toast.makeText(getActivity(), "DeviewRefresh", Toast.LENGTH_SHORT).show();
+        FriendList = new LinkedList<String>();
+        FriendList.addAll(Arrays.asList(mStrings));
 
-        new Handler().postDelayed(new Runnable() {
+        mAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,FriendList);
+
+        actualListView.setAdapter(mAdapter);
+
+        actualListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void run() {
-                mSwipeRefreshLayout.setRefreshing(false);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), "pos:" + position + " id:" + id, Toast.LENGTH_LONG).show();
             }
-        }, 3000);
+        });
     }
+
+    private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+            // Simulates a background job.
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+            }
+            return mStrings;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+
+            FriendList.add(3,"add after pos 3");
+            //customAdapter.notifyDataSetChanged();
+
+            mAdapter.notifyDataSetChanged();
+
+            // Call onRefreshComplete when the list has been refreshed.
+            mPullRefreshListView.onRefreshComplete();
+
+            super.onPostExecute(result);
+        }
+    }
+
 }
