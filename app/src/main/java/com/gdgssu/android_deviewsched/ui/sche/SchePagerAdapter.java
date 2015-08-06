fc481802c0f063207525dea3a3871ef01111e79e
Hyunseol Kim <hyunseol518@naver.com>
2015-08-06 17:15:05 +0900

    Day 1 List row 출력

--- app/src/main/java/com/gdgssu/android_deviewsched/ui/sche/SchePagerAdapter.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/ui/sche/SchePagerAdapter.java
@@ -15,6 +15,8 @@ import com.gdgssu.android_deviewsched.model.Session;
 import com.gdgssu.android_deviewsched.model.Track;
 import com.gdgssu.android_deviewsched.util.GlideCircleTransform;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 /**
@@ -59,41 +61,66 @@ public class SchePagerAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        ViewHolder holder;
+        SessionViewHolder sessionHolder;
+        DayViewHolder dayHolder;
+        if (position==0){
+            if (convertView==null){
+                convertView = mInflater.inflate(R.layout.item_day_sche_list, parent, false);
 
-        if (convertView==null){
-            convertView = mInflater.inflate(R.layout.item_session_sche_list, parent, false);
+                dayHolder = new DayViewHolder();
+                dayHolder.day = (TextView)convertView.findViewById(R.id.item_day_sche_list_day);
+                dayHolder.date = (TextView)convertView.findViewById(R.id.item_date_sche_list_day);
 
-            holder = new ViewHolder();
-            holder.sessionTime = (TextView)convertView.findViewById(R.id.item_sche_list_time);
-            holder.speakerImg = (ImageView)convertView.findViewById(R.id.item_sche_list_speaker_img);
-            holder.speakerName = (TextView)convertView.findViewById(R.id.item_sche_list_speaker_name);
-            holder.sessionName = (TextView)convertView.findViewById(R.id.item_sche_list_session_title);
+                convertView.setTag(dayHolder);
+            }else{
+                dayHolder = (DayViewHolder)convertView.getTag();
+            }
 
-            convertView.setTag(holder);
+            dayHolder.day.setText("Day1");
+            dayHolder.date.setText("9.14");
 
         }else{
-            holder = (ViewHolder)convertView.getTag();
+
+            if (convertView==null){
+                convertView = mInflater.inflate(R.layout.item_session_sche_list, parent, false);
+
+                sessionHolder = new SessionViewHolder();
+                sessionHolder.sessionTime = (TextView)convertView.findViewById(R.id.item_sche_list_time);
+                sessionHolder.speakerImg = (ImageView)convertView.findViewById(R.id.item_sche_list_speaker_img);
+                sessionHolder.speakerName = (TextView)convertView.findViewById(R.id.item_sche_list_speaker_name);
+                sessionHolder.sessionName = (TextView)convertView.findViewById(R.id.item_sche_list_session_title);
+
+                convertView.setTag(sessionHolder);
+
+            }else{
+                sessionHolder = (SessionViewHolder)convertView.getTag();
+            }
+
+            sessionHolder.sessionTime.setText(sessionTimes[position-1]);
+
+            Glide.with(DeviewSchedApplication.GLOBAL_CONTEXT)
+                    .load(sessionItems.get(position-1).speakers.get(0).img)
+                    .transform(new GlideCircleTransform(DeviewSchedApplication.GLOBAL_CONTEXT))
+                    .override(54, 54) //임의로 결정한 크기임.
+                    .into(sessionHolder.speakerImg);
+
+            sessionHolder.speakerName.setText(sessionItems.get(position-1).speakers.get(0).name);
+            sessionHolder.sessionName.setText(sessionItems.get(position-1).session_title);
+
         }
-
-        holder.sessionTime.setText(sessionTimes[position]);
-
-        Glide.with(DeviewSchedApplication.GLOBAL_CONTEXT)
-                .load(sessionItems.get(position).speakers.get(0).img)
-                .transform(new GlideCircleTransform(DeviewSchedApplication.GLOBAL_CONTEXT))
-                .override(54, 54) //임의로 결정한 크기임.
-                .into(holder.speakerImg);
-
-        holder.speakerName.setText(sessionItems.get(position).speakers.get(0).name);
-        holder.sessionName.setText(sessionItems.get(position).session_title);
 
         return convertView;
     }
 
-    public static class ViewHolder{
+    public static class SessionViewHolder{
         public TextView sessionTime;
         public ImageView speakerImg;
         public TextView speakerName;
         public TextView sessionName;
+    }
+
+    public static class DayViewHolder{
+        public TextView day;
+        public TextView date;
     }
 }
