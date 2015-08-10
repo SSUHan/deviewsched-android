6aa5d7bf87436d30ff29cb29d8c4712fae4295f3
Hyunseol Kim <hyunseol518@naver.com>
2015-08-10 20:51:28 +0900

    Detail한 세션의 정보를 볼수이ㅆ는 Activity를 만들고, 기본 토대를 만듦

---
+++ app/src/main/java/com/gdgssu/android_deviewsched/ui/detailsession/DetailSessionActivity.java
@@ -0,0 +1,107 @@
+package com.gdgssu.android_deviewsched.ui.detailsession;
+
+import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.bumptech.glide.Glide;
+import com.gdgssu.android_deviewsched.R;
+import com.gdgssu.android_deviewsched.model.Session;
+
+import java.util.ArrayList;
+
+public class DetailSessionActivity extends AppCompatActivity {
+
+    ArrayList<String> arrayList = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_session);
+
+        /**
+         * Todo 이전 세션리스트에서 Intent를 이용해 데이터를 가져와야한다.
+         */
+//
+//        Intent intent = getIntent();
+//        Session sessionInfo = (Session)intent.getSerializableExtra("KEY");
+
+        // CoolapsingView를 테스트하기위한 리스트뷰 더미데이터
+        arrayList.add("Item1");
+        arrayList.add("Item2");
+        arrayList.add("Item3");
+        arrayList.add("Item4");
+        arrayList.add("Item1");
+        arrayList.add("Item2");
+        arrayList.add("Item3");
+        arrayList.add("Item4");
+        arrayList.add("Item1");
+        arrayList.add("Item2");
+        arrayList.add("Item3");
+        arrayList.add("Item4");
+        arrayList.add("Item1");
+        arrayList.add("Item2");
+        arrayList.add("Item3");
+        arrayList.add("Item4");
+
+        initView();
+    }
+
+    private void initView() {
+
+        initToolbar();
+        loadBackdropImage();
+        initListView();
+
+    }
+
+    private void initToolbar() {
+        final Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("Session1");
+    }
+
+    private void loadBackdropImage() {
+        //임시 더미이미지
+        ImageView backdropImage = (ImageView)findViewById(R.id.backdrop);
+        Glide.with(getApplicationContext()).load("http://insanehong.kr/post/deview2013/@img/keynote.jpg")
+                .centerCrop().into(backdropImage);
+    }
+
+    private void initListView() {
+        ListView listview = (ListView)findViewById(R.id.activity_detail_session_list);
+        listview.setAdapter(new DetailSessionAdapter(getApplicationContext(), arrayList));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail_session, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
