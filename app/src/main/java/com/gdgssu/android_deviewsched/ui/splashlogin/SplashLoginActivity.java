7c1d1ffbfa569799c6937254b9adef4ace21a00b
Hyunseol Kim <hyunseol518@naver.com>
2015-08-10 18:49:21 +0900

    SplashActivity에서 전체시간표를 받아오면 FrontInfoLayout이 애니메이션으로 없어지는 기능 개발

--- app/src/main/java/com/gdgssu/android_deviewsched/ui/splashlogin/SplashLoginActivity.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/ui/splashlogin/SplashLoginActivity.java
@@ -2,9 +2,13 @@ package com.gdgssu.android_deviewsched.ui.splashlogin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.android.volley.Response;
@@ -22,11 +26,13 @@ import com.gdgssu.android_deviewsched.ui.MainActivity;
 
 import static com.navercorp.volleyextensions.volleyer.Volleyer.volleyer;
 
-public class SplashLoginActivity extends AppCompatActivity {
+public class SplashLoginActivity extends AppCompatActivity implements FacebookCallback<LoginResult> {
 
     private static final String TAG = "SplashLoginActivity";
 
     private CallbackManager callbackManager;
+
+    private LinearLayout frontInfo;
 
     /**
      * 애플리케이션에 유저 로그인이 되어있는 상태와 되어있지 않은 상태로 액티비티에서 하는 일이 달라짐
@@ -49,6 +55,10 @@ public class SplashLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashlogin);
 
+        initView();
+
+        getAllScheData();
+
         callbackManager = CallbackManager.Factory.create();
 
         if (DeviewSchedApplication.LOGIN_STATE){
@@ -57,9 +67,6 @@ public class SplashLoginActivity extends AppCompatActivity {
         }else{
 
         }
-
-
-        initView();
     }
 
     private void initView() {
@@ -68,10 +75,73 @@ public class SplashLoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                loginButton.registerCallback(callbackManager, SplashLoginActivity.this);
+            }
+        });
+
+        frontInfo = (LinearLayout)findViewById(R.id.activity_splashlogin_frontinfo);
+
+    }
+
+    private void getAllScheData() {
+        /**
+         * Todo 이곳에서 모든 전체스케쥴의 데이터를 가져오는 일을 해야함.
+         */
+        volleyer(DeviewSchedApplication.deviewRequestQueue)
+                .get(DeviewSchedApplication.HOST_URL + "mock/allsche.json")
+                .withTargetClass(AllScheItems.class)
+                .withListener(new Response.Listener<AllScheItems>() {
                     @Override
-                    public void onSuccess(LoginResult loginResult) {
-//                        Log.d("Login", "onSuccess");
+                    public void onResponse(AllScheItems items) {
+                        AllScheItems.result = items;
+
+                        frontInfoFadeout();
+                    }
+                })
+                .withErrorListener(new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, error.toString());
+                    }
+                })
+                .execute();
+    }
+
+    private void frontInfoFadeout() {
+        final Animation animationFadeout;
+
+        animationFadeout = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadeout);
+        animationFadeout.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                frontInfo.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        frontInfo.startAnimation(animationFadeout);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSuccess(LoginResult loginResult) {
+
+        //                        Log.d("Login", "onSuccess");
 //
 //                        Log.d("Token", AccessToken.getCurrentAccessToken().getToken());
 //
@@ -92,56 +162,24 @@ public class SplashLoginActivity extends AppCompatActivity {
 //                                })
 //                                .execute();
 
-                        LoginPreferenceHelper prefHelper = new LoginPreferenceHelper(DeviewSchedApplication.GLOBAL_CONTEXT);
-                        prefHelper.setPrefLoginValue(LoginPreferenceHelper.PREF_LOGIN_STATE, true);
-                        finish();
-                        startActivity(new Intent(SplashLoginActivity.this, MainActivity.class));
-                    }
+        LoginPreferenceHelper prefHelper = new LoginPreferenceHelper(DeviewSchedApplication.GLOBAL_CONTEXT);
+        prefHelper.setPrefLoginValue(LoginPreferenceHelper.PREF_LOGIN_STATE, true);
+        finish();
+        startActivity(new Intent(SplashLoginActivity.this, MainActivity.class));
 
-                    @Override
-                    public void onCancel() {
-                        Log.d("Login", "onCancel");
-
-                    }
-
-                    @Override
-                    public void onError(FacebookException e) {
-                        Log.d("Login", "onError" + e.toString());
-                    }
-                });
-            }
-        });
-    }
-
-    private void getAllScheData() {
-        /**
-         * Todo 이곳에서 모든 전체스케쥴의 데이터를 가져오는 일을 해야함.
-         */
-        volleyer(DeviewSchedApplication.deviewRequestQueue)
-                .get(DeviewSchedApplication.HOST_URL + "mock/allsche.json")
-                .withTargetClass(AllScheItems.class)
-                .withListener(new Response.Listener<AllScheItems>() {
-                    @Override
-                    public void onResponse(AllScheItems items) {
-                        AllScheItems.result = items;
-                        Toast.makeText(DeviewSchedApplication.GLOBAL_CONTEXT,
-                                AllScheItems.result.tracks.get(0).sessions.get(0).session_title,
-                                Toast.LENGTH_LONG).show();
-                    }
-                })
-                .withErrorListener(new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d(TAG, error.toString());
-                    }
-                })
-                .execute();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onCancel() {
 
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        Log.d("Login", "onCancel");
+
+    }
+
+    @Override
+    public void onError(FacebookException e) {
+
+        Log.d("Login", "onError" + e.toString());
+
     }
 }
