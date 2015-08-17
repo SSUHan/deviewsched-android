b763a9c8ac830123c48e66e1f200851e39aa5e72
Hyunseol Kim <hyunseol518@naver.com>
2015-08-17 20:12:33 +0900

    DetailSessionActivity의 적절한 마진과 뷰 위치를 잡음

--- app/src/main/java/com/gdgssu/android_deviewsched/ui/detailsession/DetailSessionActivity.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/ui/detailsession/DetailSessionActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.gdgssu.android_deviewsched.R;
@@ -19,6 +20,10 @@ import java.util.ArrayList;
 public class DetailSessionActivity extends AppCompatActivity {
 
     private ArrayList<String> arrayList = new ArrayList<String>();
+
+    private ListView listView;
+
+    private String dummyText = "대통령의 임기연장 또는 중임변경을 위한 헌법개정은 그 헌법개정 제안 당시의 대통령에 대하여는 효력이 없다. 대통령이 궐위된 때 또는 대통령 당선자가 사망하거나 판결 기타의 사유로 그 자격을 상실한 때에는 60일 이내에 후임자를 선거한다. 체포·구속·압수 또는 수색을 할 때에는 적법한 절차에 따라 검사의 신청에 의하여 법관이 발부한 영장을 제시하여야 한다. 다만, 현행범인인 경우와 장기 3년 이상의 형에 해당하는 죄를 범하고 도피 또는 증거인멸의 염려가 있을 때에는 사후에 영장을 청구할 수 있다.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,18 +72,34 @@ public class DetailSessionActivity extends AppCompatActivity {
     }
 
     private void initListView() {
-        ListView listview = (ListView) findViewById(R.id.activity_detail_session_list);
+        listView = (ListView) findViewById(R.id.activity_detail_session_list);
         if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
-            listview.setNestedScrollingEnabled(true);
+            listView.setNestedScrollingEnabled(true);
         }
 
+        initHeaderView();
+
+        initFooterView();
+
+        listView.setAdapter(new DetailSessionAdapter(getApplicationContext(), arrayList));
+    }
+
+    private void initHeaderView() {
         View headerView = getLayoutInflater().inflate(R.layout.item_detail_session_header, null, false);
+
+        TextView lectureTarget = (TextView)headerView.findViewById(R.id.item_detail_session_second_sessiontarget);
+        lectureTarget.setText("훌륭한 프로그래머가 되고싶은 모든 ");
+
+        TextView lectureContent = (TextView)headerView.findViewById(R.id.item_detail_session_second_sessioninfo);
+        lectureContent.setText(dummyText);
+
+        listView.addHeaderView(headerView);
+    }
+
+    private void initFooterView() {
         View footerView = getLayoutInflater().inflate(R.layout.item_detail_session_footer, null, false);
 
-        listview.addHeaderView(headerView);
-        listview.addFooterView(footerView);
-
-        listview.setAdapter(new DetailSessionAdapter(getApplicationContext(), arrayList));
+        listView.addFooterView(footerView);
     }
 
     @Override
